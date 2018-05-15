1059250551915e78ed6bf39692967ab66ba4a90e
Selmi11895480 <selmi.kerim@gmail.com>
2018-05-15 16:08:15 +0100

    min sum

--- MinimizeSumOfArray/solutions.js
+++ MinimizeSumOfArray/solutions.js
@@ -1,11 +1,6 @@
 function minSum(arr) {
-  // your code here
-  arr = arr.sort(function(a,b){return a - b});
-  var res= 0;
-  
-  console.log(arr);
-  for(var i=0;i<arr.length/2;i++){
-    res+=arr[i]*arr[arr.length-1-i]
-  }
-  return res
+  arr.sort((x,y)=>x-y)
+  s=0
+  while(arr.length)s+=arr.pop()*arr.shift()
+  return s
 }
