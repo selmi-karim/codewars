50c75a4630c5f41e5b09cfcbb51e614835a4a506
Selmi11895480 <selmi.kerim@gmail.com>
2018-05-19 10:53:48 +0100

    valid bracesfixed

--- ValidBraces/solutions.js
+++ ValidBraces/solutions.js
@@ -1,8 +1,10 @@
-function validParentheses(parens){
+function validBraces(parens){
   var length = parens.length / 2;
   
   for (var i = 0; i <= length; ++i) {
     parens = parens.replace('()', '');
+    parens = parens.replace('[]', '');
+    parens = parens.replace('{}', '');
   }
   
   return parens == '';
