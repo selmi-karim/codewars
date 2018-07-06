b6dce486ac9298edffebdc12cb5e399d38516137
Selmi11895480 <selmi.kerim@gmail.com>
2018-07-06 16:42:52 +0100

    arrayproduct :boom:

--- ArrayProduct/solution.js
+++ ArrayProduct/solution.js
@@ -1,9 +1,15 @@
 const BigNumber = require("bignumber.js");
-
 function ProductSansN(n) {
-  let res = n.reduce(function (accumulator, currentValue) {
-  return accumulator * currentValue;
-}, 0);
-return res
-  // your code
+  let r = new BigNumber(1), z = null;
+  for (let i = 0; i < n.length; i++) {
+    if (n[i] === 0 && z === null) {z = [i]}
+    else if (n[i] === 0) {return Array.from({length: n.length}, x => "0")}
+    else {r = r.times(new BigNumber(n[i]))}
+  }
+  if (z) {
+    let a = Array.from({length: n.length}, x => "0");
+    a[z[0]] = r.toString(10);
+    return a;
+  }
+  return n.map(x => r.dividedBy(new BigNumber(x)).toFixed(0));
 }
