3b9953536a6f7a47c50b5c9628304304452d448d
Selmi11895480 <selmi.kerim@gmail.com>
2018-07-06 17:14:21 +0100

    erathosthen

---
+++ SumOfPrime-indexedElements/solution.js
@@ -0,0 +1,24 @@
+
+let primes = []
+for (let i = 0; i <= 5000; i++) primes[i] = true
+primes[0] = primes[1] = false
+function erathosthen() {
+    for (let i = 2; i <= 5000; i++) {
+        if (primes[i]){
+            for (let j = i*2; j <= 5000; j+=i) {
+                primes[j] = false
+            }
+        }
+    }
+}
+
+function total(arr) {
+    erathosthen()
+    let res = 0;
+    arr.forEach((element,index) => {
+        if(primes[index]) res+=element
+    });
+    return res
+};
+
+total(['15','45','5'])
